e1bd106c5eacc1c576e0f397c11418e3e3624217
Timothé Bailly-Barthez <timothe.barthez@student-cs.fr>
2018-11-19 16:40:51 +0100

    ajout du comptage du nombre de tests

--- code_analyser/lines_functions_counter.py
+++ code_analyser/lines_functions_counter.py
@@ -1,26 +1,40 @@
-def lines_counter(list_of_strings) :
+def lines_counter(list_of_strings):
     """list_of_strings is obtained with the function to_list
     returns le number of lines"""
-    try :
+    try:
         assert type(list_of_strings) == list
-        c_lines = 0 #counter
-        for line in list_of_strings :
-            if line != '' : #empty lines are not counted
-                c_lines +=1
-        return(c_lines)
-    except AssertionError :
+        c_lines = 0  # counter
+        for line in list_of_strings:
+            if line != '':  # empty lines are not counted
+                c_lines += 1
+        return (c_lines)
+    except AssertionError:
         print('The parameter is not a list')
 
 
-def functions_counter(list_of_strings) :
+def functions_counter(list_of_strings):
     """list_of_strings is obtained with the function to_list
-    returns le number of functions"""
-    try :
+    returns the number of functions"""
+    try:
         assert type(list_of_strings) == list
         c_def = 0
-        for line in list_of_strings :
-            if line[0:5] == '  def' :
+        for line in list_of_strings:
+            if line[0:5] == '  def':
                 c_def += 1
         return (c_def)
-    except AssertionError :
+    except AssertionError:
+        print('The parameter is not a list')
+
+
+def tests_counter(list_of_strings):
+    """list_of_strings is obtained with the function to_list
+    returns the number of tests"""
+    try:
+        assert type(list_of_strings) == list
+        c_test = 0
+        for line in list_of_strings:
+            if line[0:6] == '  test':
+                c_test += 1
+        return c_test
+    except AssertionError:
         print('The parameter is not a list')
--- code_analyser/test_lines_functions_counter.py
+++ code_analyser/test_lines_functions_counter.py
@@ -1,10 +1,14 @@
-import pytest
 from code_analyser.lines_functions_counter import *
 
-def test_lines_counter() :
+
+def test_lines_counter():
     assert lines_counter(['  def', 'a', 'z', '  def', '']) == 4
     assert lines_counter('hello') == None
 
-def test_functions_counter() :
+
+def test_functions_counter():
     assert functions_counter(['  def', 'a', 'z', '  def', '']) == 2
 
+
+def test_tests_counter():
+    assert tests_counter(['  test', 'a', 'z', '  tests', '']) == 2

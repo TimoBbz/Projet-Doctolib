4b6329a4920b1f1bbec8c25e8f1f8e32bde326fc
Clémence ALAIN <clemence.alain@student-cs.fr>
2018-11-19 16:15:09 +0100

    fonction similarités du code

--- code_analyser/test_analyse_redundancy.py
+++ code_analyser/test_analyse_redundancy.py
@@ -4,3 +4,8 @@ from code_analyser import analyse_redundancy as ar
 def test_count_same_lines():
     code_liste=["truc","machin","super","machin"]
     assert ar.count_same_lines(code_liste)==1
+
+def test_lines_similarity():
+    assert ar.lines_similarity("coucou c'est moi","coucou c'est moi")==1
+    assert ar.lines_similarity("coucou c'est moi","coucouuu c'est moi")>0.8
+    assert ar.lines_similarity("hello","coucou")<0.2

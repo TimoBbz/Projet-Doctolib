4973a9e8e2d5ab719d378864b61a036a36620a3f
Marty Lucie <lucie.marty@student-cs.fr>
2018-11-19 14:37:08 +0100

    counter

--- code_analyser/lines_functions_counter.py
+++ code_analyser/lines_functions_counter.py
@@ -1,17 +1,25 @@
 def lines_counter(liste) :
     """paramètre : liste de chaîne de caractères, chaque élément est une ligne du code du candidat
-    compte le nombre de lignes de code"""
-    c_lignes = 0 #compteur de lignes
-    for ligne in liste :
-        if ligne != '' :
-            c_lignes +=1
-    return(c_lignes)
+    retourne le nombre de lignes de code non vides"""
+    try :
+        assert type(liste) == list
+        c_lignes = 0 #compteur de lignes
+        for ligne in liste :
+            if ligne != '' : #on ne compte pas les lignes vides
+                c_lignes +=1
+        return(c_lignes)
+    except AssertionError :
+        print('The parameter is not a list')
+
 
 def functions_counter(liste) :
     """paramètre : liste de chaîne de caractères, chaque élément est une ligne du code du candidat
-    compte le nombre de fonctions"""
-    c_def = 0 #compteur de fonctions
-    for ligne in liste :
-        c_def += ligne.count('def')
-    return (c_def)
-
+    retourne  le nombre de fonctions"""
+    try :
+        assert type(liste) == list
+        c_def = 0 #compteur de fonctions
+        for ligne in liste :
+            c_def += ligne.count('def')
+        return (c_def)
+    except AssertionError :
+        print('The parameter is not a list')
--- code_analyser/test_lines_functions_counter.py
+++ code_analyser/test_lines_functions_counter.py
@@ -3,6 +3,7 @@ from code_analyser.lines_functions_counter import *
 
 def test_lines_counter() :
     assert lines_counter(['def', 'a', 'z', 'def', '']) == 4
+    assert lines_counter('hello') == None
 
 def test_functions_counter() :
     assert functions_counter(['def', 'a', 'z', 'def', '']) == 2

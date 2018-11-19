1a36a0c077a3563fe552d9ae5b280461b085b3bd
Marty Lucie <lucie.marty@student-cs.fr>
2018-11-19 15:23:45 +0100

    counter update

--- code_analyser/lines_functions_counter.py
+++ code_analyser/lines_functions_counter.py
@@ -1,25 +1,26 @@
-def lines_counter(liste) :  
-    """paramètre : liste de chaîne de caractères, chaque élément est une ligne du code du candidat
-    retourne le nombre de lignes de code non vides"""
+def lines_counter(list_of_strings) :
+    """list_of_strings is obtained with the function to_list
+    returns le number of lines"""
     try :
-        assert type(liste) == list
-        c_lignes = 0 #compteur de lignes
-        for ligne in liste :
-            if ligne != '' : #on ne compte pas les lignes vides
-                c_lignes +=1
-        return(c_lignes)
+        assert type(list_of_strings) == list
+        c_lines = 0 #counter
+        for line in list_of_strings :
+            if line != '' : #empty lines are not counted
+                c_lines +=1
+        return(c_lines)
     except AssertionError :
         print('The parameter is not a list')
 
 
-def functions_counter(liste) :
-    """paramètre : liste de chaîne de caractères, chaque élément est une ligne du code du candidat
-    retourne  le nombre de fonctions"""
+def functions_counter(list_of_strings) :
+    """list_of_strings is obtained with the function to_list
+    returns le number of functions"""
     try :
-        assert type(liste) == list
-        c_def = 0 #compteur de fonctions
-        for ligne in liste :
-            c_def += ligne.count('def')
+        assert type(list_of_strings) == list
+        c_def = 0
+        for line in list_of_strings :
+            if line[0:3] == 'def' :
+                c_def += 1
         return (c_def)
     except AssertionError :
         print('The parameter is not a list')

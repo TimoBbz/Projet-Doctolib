ef48397ab865f6e3f4e4521ba3a9d6ce4311ee0d
Timothé Bailly-Barthez <timothe.barthez@student-cs.fr>
2018-11-19 09:54:07 +0100

    ajout du fichier extract_files.py

--- code_analyser/extract_file.py
+++ code_analyser/extract_file.py
@@ -5,3 +5,12 @@ def to_string(path):
     with open(path, 'r') as file:
         string = file.read()
     return string
+
+
+def to_list(path):
+    """Turn the file in a list of lines
+    Entry: the path of the file
+    Return: a list containing the lines of the program"""
+    string = to_string(path)
+    list = string.split(sep="\n")
+    return list

fcd15d1cea2fea98e6b11da44b42569fd576176f
Clémence ALAIN <clemence.alain@student-cs.fr>
2018-11-21 09:27:14 +0100

    prise en compte des lignes 'end'

--- code_analyser/analyse_redundancy.py
+++ code_analyser/analyse_redundancy.py
@@ -3,6 +3,8 @@ from difflib import SequenceMatcher
 def count_same_lines(code_list):
     '''count the number of redundant lines in the code
     :param code_list : liste des lignes du code'''
+
+    code_list = [item for item in code_list if item != "end"]
     code_set=set(code_list) #set permet de récupérer les items uniques
     return len(code_list)-len(code_set)
 
@@ -19,5 +21,6 @@ def code_similarity(code_list):
     somme=0
     for i in range(n):
         for j in range(i+1,n):
-            somme+=lines_similarity(code_list[i],code_list[2])
+            somme+=lines_similarity(code_list[i],code_list[j])
     return somme/(n*(n+1)/2)
+

4a585915725234c4e9dfe230264c1126ffd222e9
Bekaddour Leila <leila.bekaddour@student-cs.fr>
2018-11-21 11:44:35 +0100

    created sub dictionaries in main

--- code_analyser/__main__.py
+++ code_analyser/__main__.py
@@ -17,6 +17,7 @@ import analyse_redundancy
 import asserts_counter
 import code_coverage
 import style
+import analyse_plagiarism
 import sys
 import json
 
@@ -33,9 +34,10 @@ def from_ruby_to_json():
     # This is just to check you entered a file_path whether in the shell or as an argument
     try:
         path = sys.argv[1]
+        annales_path = sys.argv[2]
     except:
-        # If you didn't pass any argument in the sell, file_path will have this value for now
-        print("Please enter a file path, in your shell : python path_to_code_analyser path_to_your_ruby_file")
+        # If you didn't pass any argument in the sell
+        print("Please enter a file path and annals directory path, in your shell : python path_to_code_analyser path_to_your_ruby_file path_to_annals_directory")
         return None
 
     path_test = path[:len(path) - 3] + "Test" + path[len(path) - 3:]
@@ -50,57 +52,66 @@ def from_ruby_to_json():
 
     dico = {}
 
-    # number of functions and of lines
+    dico['general_numbers'] = {
 
-    dico['nb_functions'] = lines_functions_counter.functions_counter(
-        main_list_of_string)
-    dico['nb_lines'] = lines_functions_counter.lines_counter(
-        main_list_of_string)
+        'nb_comments': analyse_comments.count_all_comments(
+            main_list_of_string),
 
-    # counting tests
+        'nb_lines': lines_functions_counter.lines_counter(
+            main_list_of_string),
 
-    dico['nb_tests'] = analyse_tests.count_tests(test_string)
+        'nb_functions': lines_functions_counter.functions_counter(
+            main_list_of_string)
+    }
 
-    # counting comments
+    # information about tests
 
-    dico['nb_comments'] = analyse_comments.count_all_comments(
-        main_list_of_string)
+    dico['tests'] = {
 
-    # number of same lines
+        'nb_test_per_function': code_coverage.code_coverage_estimation(
+            main_list_of_string, test_list_of_string),
 
-    dico['nb_repeated_lines'] = analyse_redundancy.count_same_lines(
-        main_list_of_string)
+        'nb_tests': analyse_tests.count_tests(test_string),
 
-    # redundancy coeff
+        'nb_asserts_per_test': asserts_counter.asserts_counter(
+            test_list_of_string)
+    }
 
-    dico['redundancy coeff'] = analyse_redundancy.code_similarity(
-        main_list_of_string)
+    # syntax errors
 
-    # number of asserts per test in test file
+    dico['syntax_errors'] = {
 
-    dico['nb_asserts_per_test'] = asserts_counter.asserts_counter(
-        test_list_of_string)
+        'snake_case_errors': style.snake_case_function(
+            main_list_of_string),
 
-    # number of test per function, if >=1, that's good
+        'hooks_errors': style.count_solo_hooks(main_string),
 
-    dico['nb_test_per_function'] = code_coverage.code_coverage_estimation(
-        main_list_of_string, test_list_of_string)
+        'parenthesis_errors': style.count_solo_parenthesis(main_string),
 
-    # number of indentations errors
+        'indentations_errors': style.is_indent(main_list_of_string)
 
-    dico['indentations_errors'] = style.is_indent(main_list_of_string)
+    }
 
-    # parenthesis errors
+    # repetitions
 
-    dico['parenthesis_errors'] = style.count_solo_parenthesis(main_string)
+    sample_path_list = analyse_plagiarism.get_files_list(annales_path)
+    average_lines_similarity = analyse_plagiarism.average_lines_similarity(
+        main_list_of_string, sample_path_list)
 
-    # hooks errors
+    dico['repetitions'] = {
 
-    dico['hooks_errors'] = style.count_solo_hooks(main_string)
+        'nb_repeated_lines': analyse_redundancy.code_similarity(
+            main_list_of_string),
 
-    # snake case errors in name of functions
+        'redundancy_coeff': analyse_redundancy.code_similarity(
+            main_list_of_string),
 
-    dico['snake_case_errors'] = style.snake_case_function(main_list_of_string)
+        'plagiarism': {
+            "closest_file": average_lines_similarity[0],
+            "similarity_w_closest_file": average_lines_similarity[1],
+            "average_similarity": average_lines_similarity[2]
+        }
+    }
 
     with open(path[:len(path)-3]+"Results.json", "w+") as my_file:
         json.dump(dico, my_file, indent=4)

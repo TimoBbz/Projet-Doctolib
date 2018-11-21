32d83f5daf50bea4742ed84d4917d33c8032709d
Bekaddour Leila <leila.bekaddour@student-cs.fr>
2018-11-21 15:02:22 +0100

    adding some test

--- code_analyser/test_analyse_plagiarism.py
+++ code_analyser/test_analyse_plagiarism.py
@@ -3,6 +3,11 @@ from code_analyser import analyse_plagiarism as ap
 
 
 def test_average_lines_similarity():
+    """
+
+    Testing average_lines_similarity
+
+    """
     code_list = ["  validates :kind, inclusion: { in: KIND, message: 'is not a valid kind of event' }",
                  "  validates :starts_at, presence: true", "  validates :ends_at, presence: true"]
     assert ap.average_lines_similarity(code_list, [
@@ -12,5 +17,10 @@ def test_average_lines_similarity():
 
 
 def test_get_files_list():
+    """
+
+    Testing get_files_list
+
+    """
     assert ap.get_files_list("PastDatas") == [
         'EventCandidatC.rb', 'EventCandidatB.rb']

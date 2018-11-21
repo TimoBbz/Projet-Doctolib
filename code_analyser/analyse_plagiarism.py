77dd490f645379b2c0c61ceca4db11e58e9c8960
Clémence ALAIN <clemence.alain@student-cs.fr>
2018-11-21 10:48:08 +0100

    infos du récopié en plus

--- code_analyser/analyse_plagiarism.py
+++ code_analyser/analyse_plagiarism.py
@@ -1,6 +1,7 @@
 from code_analyser import extract_file as ef
 from os import listdir
 from os.path import isfile, join
+import numpy as np
 
 
 
@@ -11,6 +12,7 @@ def average_lines_similarity(code_list, samplepathlist):
 
     similarity=0
     code_list=[item for item in code_list if (item[-3:]!="end")] #does not compare the number of lines which are just end of blocks
+    similarity=[]
     for samplepath in samplepathlist:
         code_list2=ef.to_list(samplepath)
         code_list2=[item for item in code_list2 if (item[-3:]!="end")]
@@ -18,8 +20,9 @@ def average_lines_similarity(code_list, samplepathlist):
         for line in code_list:
             if line in code_list2:
                 compteur+=1
-        similarity+=compteur/len(code_list)/len(samplepathlist)
-    return similarity*100
+        similarity.append(compteur/len(code_list))
+
+    return samplepathlist[similarity.index(max(similarity))],max(similarity),np.mean(similarity)
 
 
 def get_files_list(directorypath):

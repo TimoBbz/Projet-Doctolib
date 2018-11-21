15f06c3dd1cead5cc353002995baa86fdcdccbc5
Clémence ALAIN <clemence.alain@student-cs.fr>
2018-11-21 10:11:00 +0100

    ajout fonction antiplagiat

---
+++ code_analyser/analyse_plagiarism.py
@@ -0,0 +1,20 @@
+from code_analyser import extract_file as ef
+
+def average_lines_similarity(code_list, samplepathlist):
+    '''return the average pourcentage of same lines between two documents
+    :param code_list : list of lines of the code to analyse
+    :param samplepathlist : list of paths which you want to compare the code to'''
+
+    similarity=0
+    code_list=[item for item in code_list if (item[-3:]!="end")] #does not compare the number of lines which are just end of blocks
+    for samplepath in samplepathlist:
+        code_list2=ef.to_list(samplepath)
+        code_list2=[item for item in code_list2 if (item[-3:]!="end")]
+        compteur=0
+        for line in code_list:
+            if line in code_list2:
+                compteur+=1
+        print(code_list)
+        print(len(code_list),len(samplepathlist))
+        similarity+=compteur/len(code_list)/len(samplepathlist)
+    return similarity*100
---
+++ code_analyser/test_analyse_plagiarism.py
@@ -0,0 +1,6 @@
+import pytest
+from code_analyser import analyse_plagiarism as ap
+
+def test_average_lines_similarity():
+    code_list=["  validates :kind, inclusion: { in: KIND, message: 'is not a valid kind of event' }","  validates :starts_at, presence: true","  validates :ends_at, presence: true"]
+    assert ap.average_lines_similarity(code_list,["../Examples/EventCandidatA.rb"])==100

a8e678173b87fb49297a81176e1c65813a94b661
Clémence ALAIN <clemence.alain@student-cs.fr>
2018-11-22 10:17:47 +0100

    réorganisation serveurs

---
+++ server/static/results.js
@@ -0,0 +1,71 @@
+file="file://../uploads/EventCandidatAScores.json"
+
+function readTextFile(file)
+{
+	var str = "";
+	var txtFile = new File(file);
+	txtFile.open("r");
+	while (!txtFile.eof) {
+		// read each line of text
+		str += txtFile.readln() + "\n";
+	}
+return str;
+}
+
+    var datas = readTextFile(file);
+
+    var myContext = document.getElementById("myChart");
+    var myChart = new Chart(myContext, {
+      type: 'bar',
+      data: {
+        labels: ["Qualité des tests", "Absence de plagiat", "Qualité de la syntaxe"],
+        datasets: [
+           {
+           label: "Candidat 1",
+           data: [6, 4, 3],
+           backgroundColor: 'rgba(255, 99, 132, 0.2)'
+           },{
+           label: "Candidat 2",
+           data: [1,2,5],
+           backgroundColor: 'rgba(54, 162, 235, 0.2)'
+           },{
+           label: "Candidat 3",
+           data: [4,1,2],
+           backgroundColor: 'rgba(255, 206, 86, 0.2)'
+           }
+        ],
+      },
+      options:{
+         title:{
+           display:true,
+           text:"Résultats des candidats",
+           fontSize:40,
+           fontStyle:'bold',
+           padding:40
+         },
+          scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+          },
+          legend:{
+            display:true,
+            position:'right',
+            labels:{
+              fontSize:20,
+              padding:10
+            }
+          },
+          layout: {
+            padding: {
+                left: 50,
+                right: 50,
+                top: 50,
+                bottom: 50
+            }
+          }
+        }
+      }
+    )

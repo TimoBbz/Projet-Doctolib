2ee711d88685e9eda436cbf2208fde3de41cc85b
chiahetcho <44137047+chiahetcho@users.noreply.github.com>
2018-11-22 15:53:02 +0100

    delete script.py

--- server/script.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def launch_tests(file_name):
-    path_skeleton = "uploads/" + file_name
-    file_path = path_skeleton + ".rb"
-    os.system("../code_analyser file_path")
-    json_path = path_skeleton + "Results.json"
-    os.system("../code_score json_path")
